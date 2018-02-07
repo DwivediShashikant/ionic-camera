f27c6969b95314072704a5ca102bd1fc012ca906
DwivediShashikant <dwivedi.sk@outlook.com>
2018-02-07 12:02:14 +0530

    add camera picture to Camera Page

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Camera, CameraOptions } from '@ionic-native/camera'
+import { Camera, CameraOptions} from  '@ionic-native/camera'
 
-@IonicPage()
 @Component({
   selector: 'page-camera',
   templateUrl: 'camera.html',
@@ -11,29 +10,31 @@ export class CameraPage {
 
   imgSrc : any;
 
-  constructor(
+  constructor
+  (
     public navCtrl: NavController, 
     public navParams: NavParams, 
-    private _camera : Camera) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CameraPage');
+    private camera : Camera) {
   }
 
   takeSnapShot(){
 
     let options : CameraOptions = {
-      destinationType : this._camera.DestinationType.FILE_URI,
-      sourceType : this._camera.PictureSourceType.CAMERA,
-      encodingType : this._camera.EncodingType.PNG,
+      destinationType : this.camera.DestinationType.FILE_URI,
+      sourceType : this.camera.PictureSourceType.CAMERA,
+      encodingType : this.camera.EncodingType.PNG,
       targetHeight : 500,
       targetWidth : 500,
       saveToPhotoAlbum : false
     }
-    this._camera.getPicture ( options ).then( (imgSrc) => {
+
+    this.camera.getPicture( options).then( (imgSrc) => {
       this.imgSrc = imgSrc;
-    })
+    });
+
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CameraPage');
   }
 
 }
